493ca311924e49e169d17e0cd47951b026c82a81
saimahith <saimahith20@gmail.com>
2025-03-31 16:22:38 -0400

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:saibaba786@localhost/student_portal_db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
